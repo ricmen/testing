2ab8db567d22dba089fc9a47eeb9a86ab07006db
ricmen <rmendoza@nearbpo.com>
2013-10-28 19:33:16 -0700

    Adding CSS for the site, adding html code

--- index.php
+++ index.php
@@ -2,3 +2,17 @@
 	
 	echo 'This is my first app in github';
 
+	?>
+
+<!DOCTYPE html>
+	<html>
+	<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<title>This is the title</title>
+			<link rel="stylesheet" href="">
+	</head>
+	<body>
+		
+	</body>
+</html>
